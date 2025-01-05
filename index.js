e3aee93c38b9e0ed95010de18af2eebf148c961b
JessIkino <jessicaikino@icloud.com>
2025-01-05 12:08:45 -0500

    updated the html/js file
    Added to the html file the moment time zone according to continents.
    added to the JS file function that would update the city when ever we click on the select option

--- index.js
+++ index.js
@@ -5,9 +5,14 @@ let date = moment().format("MMMM Do YYYY");
 let time = moment().tz("America/Los_Angeles")
 
 let losAngelesDateElement = document.querySelector(".date");
-losAngelesDateElement.innerHTML = `${date}`;
 let losAngelesTimeElement = document.querySelector(".time");
-losAngelesTimeElement.innerHTML = time.format(" h:mm:ss [<small>]A[<small>]");
+if (losAngelesDateElement && losAngelesTimeElement) {
+
+       losAngelesDateElement.innerHTML = `${date}`;
+       losAngelesTimeElement.innerHTML = time.format(" h:mm:ss [<small>]A[<small>]");
+};
+
+
 // Paris
 let parisDate = moment().format("MMMM Do YYYY");
 let parisTime = moment().tz("Europe/Paris");
@@ -24,4 +29,28 @@ if (parisDateElement && parisTimeElement) {
 
 updateTime()
 setInterval(updateTime, 1000)
+
+function updateCity(event) {
+    let cityTimeZone = event.target.value;
+    let cityTime = moment().tz(cityTimeZone);
+    let citiesElement = document.querySelector("#cities");
+    citiesElement.innerHTML = cityTimeZone;
+    let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+
+    citiesElement.innerHTML = `
+    <div class="city">
+      <div>
+        <h2>${cityName}</h2>
+        <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
+      </div>
+      <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
+      "A"
+    )}</small></div>
+    </div>
+    `;
+
+}
+
+let selectcityElement = document.querySelector("#city")
+selectcityElement.addEventListener("change", updateCity);
 
